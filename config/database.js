14176720f5b9a39bb5738753b9a82d4700bf1a98
João Guedes <joaoguedes1404@gmail.com>
2024-07-25 20:11:20 -0300

    Mongo DÊBI

--- config/database.js
+++ config/database.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://mongo_db/todo-lists")
+mongoose.connect("mongodb://localhost/todo-lists")
     .then(() => console.log("Conectado ao MongoDB"))
     .catch((err) => console.log(err));
