96a9fc277a9322d9073fd17a66c4a02f1e916509
João Guedes <joaoguedes1404@gmail.com>
2024-07-25 20:31:55 -0300

    Alterando url mongo

--- config/database.js
+++ config/database.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://localhost/todo-lists")
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/todo-lists';
+
+mongoose.connect(mongoUrl)
     .then(() => console.log("Conectado ao MongoDB"))
     .catch((err) => console.log(err));
