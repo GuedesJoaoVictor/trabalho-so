c339fcb5602c153ca2d67e2a58b8ed97eca8e8fb
João Guedes <joaoguedes1404@gmail.com>
2024-07-24 21:58:08 -0300

    Trocando rota

--- config/database.js
+++ config/database.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://mongodb:27017/todolist", { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect("mongodb://mongodb:27017/todolist")
     .then(() => console.log("Conectado ao MongoDB"))
     .catch((err) => console.log(err));
